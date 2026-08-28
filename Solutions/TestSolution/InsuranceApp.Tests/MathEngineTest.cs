6cdf536b2a93a77f9055463a1952527b9c08cb35
Ravi Tambade <ravi.tambade@transflower.in>
2026-08-28 10:39:52 +0530

    from business to solution

---
+++ Solutions/TestSolution/InsuranceApp.Tests/MathEngineTest.cs
@@ -0,0 +1,34 @@
+﻿namespace InsuranceApp.Tests;
+using Xunit;
+using InsuranceApp;
+
+public class MathEngineTest
+{
+
+
+    //Unit Test
+
+    [Theory]
+    [InlineData(10, 20, 30)]
+    [InlineData(56, 65, 121)]
+    [InlineData(100, 50, 150)]
+    [InlineData(10, -5, 5)]
+    [InlineData(-10, -20, -30)]
+    [InlineData(0, 10, 10)]
+    [InlineData(0, 0, 0)]
+    public void Addition_ShouldReturnExpectedResult(
+        int a,
+        int b,
+        int expected)
+    {
+        // Arrange
+        MathEngine math = new MathEngine();
+
+        // Act
+        int actual = math.Addition(a, b);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+}
---
+++ Solutions/TestSolution/InsuranceApp/MathEngine.cs
@@ -0,0 +1,24 @@
+﻿namespace InsuranceApp;
+
+public class MathEngine
+{
+     public int Addition( int op1, int op2)
+    {
+        return op1+op2;
+    }
+
+    public int Subtraction(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Multiplication(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int Division(int a, int b)
+    {
+        return a / b;
+    }
+}
